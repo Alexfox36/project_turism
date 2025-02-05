0a71f9e0249d30b33d5d356d5de7ccaf63c1573c
AVKluev <klyuev_aleksandr@mail.ru>
2025-02-05 13:05:44 +0300

    first commit work

---
+++ project_turism/app/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from app.models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'cover',
+        ]
